7ef0d663749a9b4df775c54c40a8a84768eceae2
kenjikatooo <k.524.10nis@gmail.com>
2018-09-12 21:13:14 +0900

    ダッシュボードを見れるようにしたよよよよっよっよよよy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+
+  # sidekiqのダッシュボード設定
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == 'basicuser' && password == 'basicpass'
+  end
+  mount Sidekiq::Web => '/sidekiq'
+
   resources :snippets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
