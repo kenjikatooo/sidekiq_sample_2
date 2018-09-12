e868692abaa3e229c3c8233c1cea756cfa60c415
kenjikatooo <k.524.10nis@gmail.com>
2018-09-12 22:29:19 +0900

    本番用の設定を修正

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,15 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379', namespace: 'test_sidekiq' }
+  if Rails.env.production?
+    config.redis = { url: ENV["REDISTOGO_URL"], namespace: 'production_sidekiq'}
+  else
+    config.redis = { url: 'redis://localhost:6379', namespace: 'test_sidekiq' }
+  end
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379', namespace: 'test_sidekiq' }
+  if Rails.env.production?
+    config.redis = { url: ENV["REDISTOGO_URL"], namespace: 'production_sidekiq'}
+  else
+    config.redis = { url: 'redis://localhost:6379', namespace: 'test_sidekiq' }
+  end
 end
